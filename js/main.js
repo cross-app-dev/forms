630856a4162ead09f227c4e2a6d96d14a28a2a72
Wael Showair <show0017@algonquinlive.com>
2014-09-22 12:31:27 -0400

    access form element nodes in diff ways

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ function init(){
     //document.getElementById("btnSubmit").addEventListener("click", validate);
     document.getElementById("btnSubmit").onclick = validate;
 
+    var formElemNode = document.forms.myformName;
+    var sameFormElemNode = document.forms.myformID;
+    var preferredFormElemNode = document.getElementById("myformID");
+
+    console.log("access form element node using name property: ", formElemNode);
+    console.log("access from element node using id property: ", sameFormElemNode);
+    console.log("access form element node using getElementByID: ", preferredFormElemNode);
 }
 
 function validate( ){
